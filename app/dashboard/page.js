252d42426fbcdb8450eb5dee7a667ae985f98f95
Victor Ogaba <ogabavik@gmail.com>
2024-10-09 13:50:20 +0100

    UI design plans for dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,6 +3,9 @@ export default function () {
     return (
         <main>
             <p>User dashboard</p>
+            <p>Propose a UI design</p>
+            <p>Build the UI</p>
+            <p>Propose possible changes</p>
         </main>
     )
 }
