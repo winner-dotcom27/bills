f24a257536e6204bab482f98cf74837e73f60389
Victor Ogaba <ogabavik@gmail.com>
2024-10-09 13:44:41 +0100

    my project

---
+++ app/about-us/page.js
@@ -0,0 +1,67 @@
+
+
+export default function AboutUs () {
+    return (
+        <div className=" bg-black px-3 md:px-8 lg:px-16 pt-20">
+            <h1 className="font-bold text-6xl text-center text-white">We are providing an <br></br>education that <br></br>empowers</h1>
+            <p className="text-2xl pt-2 text-center text-white">Early Code empowers and creates the platform for <br></br>experts to teach everyone technical skills that count.</p>
+
+            <div className="">
+                <div className="flex justify-center gap-60">
+                    <img src="https://via.placeholder.com/150" className="pt-12 " alt=""></img>
+                    <img src="https://via.placeholder.com/150" className="pt-12 " alt=""></img>
+                    <img src="https://via.placeholder.com/150" className="pt-12 " alt=""></img>
+                </div>
+                
+                <div className="flex text-center gap-20 ml-24 text-white">
+                    <p className="">We understand that prospective students <br></br>want to achieve master, so we provided<br></br>experienced and skilled instructors</p> 
+  
+                    <p>Since technology or software development <br></br>needs vibrant collaboration, we created<br></br> physical classes for instructor-students<br></br> collaboration</p>  
+
+                    <p>We understand that study materials can <br></br>help the student raise the bar, so we<br></br> created after-class study contents</p>
+                </div>
+
+                <div className="text-center pt-14">
+                    <h2 className="text-4xl font-bold text-blue-400">Our Passion and Excellence</h2>
+                    <p  className="text-white">In Early Code our passion is to coach and help people to acquire the tech skills they need to excel. As a tech institution, we strive to make tech more <br></br>approachable and most importantly, applicable to the students. We focus more on practical skills to ensure our learners and trainees are well equipped <br></br>and proficient in their chosen paths. Courses are designed to be project-based to enable participants gain lots of experience even while in training.</p>
+                </div>
+                
+
+                <div className="text-center pt-14">
+                    <h2 className="text-4xl font-bold text-blue-400">We Have Seen That Our Work Counts</h2>
+                    <p className="text-white">Some of our graduates now work with some reputable organizations while some have been able to create enterprise opportunities for themselves. Our <br></br>principal goal is to create sufficient software developers and tech talents in Nigeria and other African countries.</p>
+                </div>
+
+                <div className="flex gap-10">
+                    <div className="bg-gray-500 w-[280px] h-[260px] rounded-xl my-20">
+                        <p className="text-2xl font-bold pt-8 ml-8">more than </p>
+                        <p className="ml-8 font-bold text-7xl pt-6">1,500 </p>
+                        <p className="text-2xl font-bold ml-8 pt-6">students trained</p>
+                    </div>
+
+                    <div className="bg-gray-500 w-[280px] h-[260px] rounded-xl my-20">
+                        <p className="text-2xl font-bold pt-8 ml-8">more than </p>
+                        <p className="ml-8 font-bold text-7xl pt-6">1,500 </p>
+                        <p className="text-2xl font-bold ml-8 pt-6">students trained</p>
+                    </div>
+
+                    <div className="bg-gray-500 w-[280px] h-[260px] rounded-xl my-20">
+                        <p className="text-2xl font-bold pt-8 ml-8">more than </p>
+                        <p className="ml-8 font-bold text-7xl pt-6">1,500 </p>
+                        <p className="text-2xl font-bold ml-8 pt-6">students trained</p>
+                    </div>
+
+                    <div className="bg-gray-500 w-[280px] h-[260px] rounded-xl my-20">
+                        <p className="text-2xl font-bold pt-8 ml-8">more than </p>
+                        <p className="ml-8 font-bold text-7xl pt-6">1,500 </p>
+                        <p className="text-2xl font-bold ml-8 pt-6">students trained</p>
+                    </div>
+                </div>
+              
+              
+            </div>
+           
+        </div>
+
+    )
+}
---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+
+export default function () {
+    return (
+        <main>
+            <p>User dashboard</p>
+        </main>
+    )
+}
